e097889ecd4e7e8a7822e99fa08fac47d2a2b1e3
molome keys <ouzar.merouane@gmail.com>
2022-11-22 21:45:44 -0500

    added the crypto card container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import CryptoCard from './component/CryptoCard';
 function App() {
+  const [isData, setIsData] = useState([]);
+  useEffect(() => {
+    fetch('https://api.coinstats.app/public/v1/coins?skip=0&limit=30&currency=USD').then((res) => res.json()).then((data) => {
+
+      setIsData(data.coins);
+    })
+  }, [])
+  const allElement = isData?.slice(0, 4).map((e) => {
+    return <CryptoCard key={e.id} name={e.name}
+      price={e.price} icon={e.icon}
+    />
+  })
+  console.log(allElement);
+  console.log(isData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='cryptoCardContainer'>
+
+        {allElement}
+      </div >
     </div>
   );
 }
---
+++ src/component/CryptoCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+export default function CryptoCard(props) {
+    return (
+        <div className='cryptoCard'>
+            <div className='cryptoCard--title'>
+
+                <img src={props.icon} />
+                <h3>{props.name} </h3>
+
+            </div>
+            <p><b>{props.price}</b></p>
+        </div>
+
+    )
+}
